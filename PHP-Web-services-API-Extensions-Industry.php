b5c177663d25b91fee798927e87b901008bc0dca
shaileshprajapati1 <shaileshprajapati966@gmail.com>
2023-10-25 21:16:37 +0530

    data

--- PHP-Web-services-API-Extensions-Industry.php
+++ PHP-Web-services-API-Extensions-Industry.php
@@ -12,6 +12,7 @@
     <title>PHP WEB SERVICES API</title>
 </head>
 
+
 <body>
     <div class="container mt-1">
         <h3>What is API?</h3>
@@ -25,10 +26,36 @@
         <p><b>Open APIs </b> are open-source application programming interfaces you can access with the HTTP protocol. Also known as <b>public APIs,</b> they have defined API endpoints and request and response formats.</p>
         <p><b>Partner APIs </b> connect strategic business partners. Typically, developers access these APIs in self-service mode through a public API developer portal. Still, they need to complete an onboarding process and get login credentials to access partner APIs.</p>
         <p><b>Internal APIs </b> remain hidden from external users. These private APIs aren't available for users outside of the company and are instead intended to improve productivity and communication across different internal development teams.</p>
-        <p><b>Composite APIs </b> combine multiple data or service APIs. They allow programmers to access several endpoints in a single call. Composite APIs are useful in microservices architecture where performing a single task may require information from several sources.
+        <p><b>Composite APIs </b> combine multiple data or service APIs. They allow programmers to access several endpoints in a single call. Composite APIs are useful in microservices architecture where performing a single task may require information from several sources.</p>
 
-        </p>
+        <h3>API protocols?</h3>
+        <p><b>SOAP (Simple Object Access Protocol): </b>Built with XML, SOAP enables endpoints to send and receive data through SMTP and HTTP. SOAP APIs make it easier to share information between apps or software components that are running in different environments or written in different languages.</p>
+        <p><b>XML-RPC (XML-Remote Procedure Call):</b> The XML-RPC protocol relies on a specific XML format to transfer data. XML-RPC is older than SOAP, but much simpler, and relatively lightweight in that it uses minimum bandwidth.</p>
+        <p><b>JSON-RPC: </b> Like XML-RPC, JSON-RPC is a remote procedure call, but JSON (JavaScript Object Notation) is used instead of XML to transfer the data.</p>
+        <p><b>REST (Representational State Transfer):</b> REST is a set of web API architecture principles. REST APIs—also known as a RESTful API—are APIs that adhere to certain REST architectural constraints. It's possible to build RESTful APIs with SOAP protocols, but the two standards are usually viewed as competing specifications.</p>
+        <p>Traditionally, API referred to an interface connected to an application created with any of the low-level programming languages, such as Javascript. Modern APIs, however, adhere to REST principles and the JSON format.</p>
 
+        <ul>
+            <h3> REST Constraints </h3>
+            The following constraints define the RESTfulness of an application or service.
+            <li>Client-Server architecture</li>
+            <li>Statelessness</li>
+            <li>Uniform interface</li>
+            <li>Layered system</li>
+            <li>Cacheability</li>
+            <li>Code on Demand</li>
+        </ul>
+
+        <ul>
+            <h3>Uses of RESTful API</h3>
+            <li>As an interface with multi-platform support which is used to access resources from outside applications coded in various programming languages like PHP, JAVA, Android and more.</li>
+            <li>REST is the simple architectural style for transmitting data over HTTP.</li>
+            <li>The REST API is the most suitable resource provider for an AJAX-based application interface which requires data to update UI without page reload.</li>
+            <li>By meeting more of the REST constraints, the web applications or services can support a wide range of clients.</li>
+            <li>The service request parameters can be sent via GET, PUT, and POST request methods.</li>
+
+        </ul>
+      
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
